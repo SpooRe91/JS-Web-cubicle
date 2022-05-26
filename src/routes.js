5c4cc4436494651fc359d4932f3bb4a32c09b531
Martin B <m.bogdanov9110@gmail.com>
2022-05-26 20:03:24 +0300

    added partial for home page and added js

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const homeController = require('./controllers/homeController');
 const createController = require('./controllers/createController');
-const aboutController = require('./controllers/aboutController');
+const aboutController = require('./controllers/homeController');
 
 const router = express.Router();
 
 router.get('/', homeController.index);
 router.get('/about', aboutController.about);
-
 router.use('/cube', createController);
-
-
 
 module.exports = router;
 
