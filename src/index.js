b6650d153b5c7896ab2f0853dc5044071730600d
Martin B <m.bogdanov9110@gmail.com>
2022-05-26 18:59:11 +0300

    added public styles and images

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 const port = 5000;
 
 const app = express();
 
 app.use('/static', express.static('public'));
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'//!!!VERY IMPORTANT
+}));
+app.set('view engine', 'hbs');
+app.set('views', '../src/views');
+
 app.get("/", (req, res) => {
-    res.send('Hello world')
+    res.render('index')
 })
 
 app.listen(port, console.log(`Listening on port http://localhost:${port}`))
