e7666ccd79b53fe80c4feae21d32600ef777e050
Martin B <m.bogdanov9110@gmail.com>
2022-05-26 22:14:40 +0300

    added search function

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,12 +1,15 @@
 const router = require('express').Router();
-const cubes = require('../db.json');
+const cubeService = require('../services/cubeService');
 
 router.get('/', (req, res) => {
+    let { search, from, to } = req.query;
+    const cubes = cubeService.getAll(search, from, to).filter(x => x.name)
     res.render('index', { cubes });
 });
 
 router.get('/about', (req, res) => {
     res.render('about')
 });
+router.get('/search')
 
 module.exports = router;
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,10 +1,20 @@
 const fs = require('fs/promises');
 const cubes = require('../db.json');
 
-exports.save = (cube) => {
-    cubes.push(cube)
-    let textData = JSON.stringify(cubes, '', 4);
-    return fs.writeFile(('./src/db.json'), textData, { encoding: 'utf-8' })
+exports.getAll = (search = "", from = 0, to = 6) => {
+    const result = cubes
+        .filter(x => x.name.toLowerCase().includes(search.toLowerCase()))
+        .filter(x => x.difficultyLevel >= from && x.difficultyLevel <= to);
+
+    return result;
 };
 
 exports.getOne = (cubeId) => cubes[cubeId];
+
+exports.save = (cube) => {
+    cubes.push(cube)
+    let textData = JSON.stringify(cubes, '', 4);
+
+    return fs.writeFile(('./src/db.json'), textData, { encoding: 'utf-8' })
+};
+
